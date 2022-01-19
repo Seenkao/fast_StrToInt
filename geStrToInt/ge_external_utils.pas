50e149abbb6dd915b0bbe6f98c26e971d3474553
Mirrel <mirrel@DevuanMy>
2022-01-19 21:22:40 +0300

    New

---
+++ geStrToInt/ge_external_utils.pas
@@ -0,0 +1,669 @@
+(*
+ *  Copyright (c) 2021-2022 Serg Shutkin
+ *
+ *  This software is provided 'as-is', without any express or
+ *  implied warranty. In no event will the authors be held
+ *  liable for any damages arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any purpose,
+ *  including commercial applications, and to alter it and redistribute
+ *  it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented;
+ *     you must not claim that you wrote the original software.
+ *     If you use this software in a product, an acknowledgment
+ *     in the product documentation would be appreciated but
+ *     is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such,
+ *     and must not be misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any
+ *     source distribution.
+ *)
+
+// Rus: требуется проверка при работе с флагами переполнения. На данный момент
+//      проблем не возникало.
+// Eng: validation is required when dealing with overflow flags. At the moment
+//      there have been no problems.
+
+unit ge_external_Utils;
+// Rus: этот дефайн UP_CPU повышает уровень используемых значений. Точнее для
+//      CPU16 вы сможете использовать 32-х битные значения. Вы должны знать
+//      может система (компилятор) использовать эти значения или нет.
+//      Для 64-х битных систем не будет работать на данное время.
+// Eng: this UP_CPU define raises the level of values used. More precisely, for
+//      CPU16 you can use 32-bit values. You need to know whether the system
+//      (compiler) can use these values or not.
+//      Will not work for 64-bit systems at this time.
+
+{.$Define UP_CPU}
+{$I def_stringtovalue.cfg}
+
+interface
+
+const
+  // Rus: константы задаваемые при вызове geStrToInt
+  // Eng: constants set when calling geStrToInt
+  isByte      = 0;
+  isShortInt  = 0;
+  {$IfDef USE_CPU16}
+  isWord      = 1;
+  isSmallInt  = 1;
+  {$EndIf}
+  {$IfDef USE_CPU32}
+  isLongWord  = 2;
+  isInteger   = 2;
+  {$EndIf}
+  {$IfDef USE_CPU64}
+  isQWord     = 3;
+  isInt64      = 3;
+  {$EndIf}
+  {$IfDef CPU8}
+  maxSize     = 0;
+  {$EndIf}
+  {$IfDef CPU16}
+  maxSize     = 1;
+  {$EndIf}
+  {$IfDef CPU32}
+  maxSize     = 2;
+  {$EndIf}
+  {$IfDef CPU64}
+  maxSize     = 3;
+  {$EndIf}
+
+type
+  {$IfDef CPU8}
+  maxIntVal = {$IfNDef UP_CPU}ShortInt{$Else}SmallInt{$EndIf};
+  maxUIntVal = {$IfNDef UP_CPU}Byte{$Else}Word{$EndIf};
+  {$EndIf}
+  {$IfDef CPU16}
+  maxIntVal = {$IfNDef UP_CPU}SmallInt{$Else}Integer{$EndIf};
+  maxUIntVal = {$IfNDef UP_CPU}Word{$Else}LongWord{$EndIf};
+  {$EndIf}
+  {$IfDef CPU32}
+  maxIntVal = {$IfNDef UP_CPU}Int64{$Else}Integer{$EndIf};
+  maxUIntVal = {$IfNDef UP_CPU}LongWord{$Else}QWord{$EndIf};
+  {$EndIf}
+  {$IfDef CPU64}
+  maxIntVal = Int64;
+  maxUIntVal = QWord;
+  {$EndIf}
+
+  geUseParametr = record
+    maxLen: LongWord;
+    maxNumDiv10: maxUIntVal;
+    maxNumeric: maxUIntVal;
+  end;
+
+  geHOBParameter = record
+    maxLen16, maxLen8, maxLen2: maxUIntVal;
+  end;
+
+// Rus: эта функция для десятичных чисел со знаком.
+// Eng: this function is for signed decimal numbers.
+function geStrToInt(const Str: String; out Value: maxIntVal; Size: LongWord = isInteger): Boolean;
+// Rus: эта функция только для десятичных чисел без знака!!!
+// Eng: this function is only for unsigned decimal numbers!!!
+function geStrToUInt(const Str: String; out Value: maxUIntVal; Size: LongWord = isLongWord): Boolean;
+// Rus: для работы с шестнадцатеричными, восьмеричными и двоичными данными.
+// Eng: for working with hexadecimal, octal and binary data.
+function geHOBStrToUInt(const Str: String; out Value: maxUIntVal; Size: LongWord = isQWord): Boolean;
+
+(* Rus: Ниже реализованы стандартные функции для перевода строк в число. Их
+ *      использование будет проще для большинства. Функции отмечены префиксом.
+ *      s_ - функции возвращают результат (если операция была неудачной, то
+ *      в результате вернётся ноль, но вы не узнаете, что операция была неудачной).
+ *      sc_ - результат функций удачная или не удачная была операция. Сам
+ *      конечный числовой результат считывайте в Value.
+ * Eng: The standard functions for converting strings to numbers are implemented
+ *      below. Their use will be easier for most. Functions are marked with a prefix.
+ *      s_ - functions return a result (if the operation was unsuccessful, the result
+ *      will be zero, but you will not know that the operation was unsuccessful).
+ *      sc_ - the result of the functions - the operation was successful or
+ *      unsuccessful. Read the final numerical result in Value.
+ *)
+// sc_ - speed + check
+// s_ - speed (not check)
+
+// Rus: Числа со знаком. Здесь нельзя использовать шестнадцатеричные, восьмеричные
+//      и двоичные числа.
+// Eng: Signed numbers. Hexadecimal, octal and binary numbers cannot be used here.
+function sc_StrToShortInt(const Str: String; out Value: ShortInt): Boolean;    // byte
+function s_StrToShortInt(const Str: String): ShortInt;                         // byte
+{$IfDef USE_CPU16}
+function sc_StrToSmallInt(const Str: String; out Value: SmallInt): Boolean;    // word
+function s_StrToSmallInt(const Str: String): SmallInt;                         // word
+{$EndIf}
+{$IfDef USE_CPU32}
+function sc_StrToInt(const Str: String; out Value: Integer): Boolean;
+function s_StrToInt(const Str: String): Integer;
+{$EndIf}
+{$IfDef USE_CPU64}
+function sc_StrToInt64(const Str: String; out Value: Int64): Boolean;
+function s_StrToInt64(const Str: String): Int64;
+{$EndIf}
+// Rus: Числа без знака. Эти функции могут использоваться и для шестнадцатеричныи
+//      и восьмеричных и двоичных чисел.
+// Eng: Numbers without a sign. These functions can be used for hexadecimal, octal
+//      and binary numbers as well.
+function sc_StrToByte(const Str: String; out Value: Byte): Boolean;
+function s_StrToByte(const Str: String): Byte;
+{$IfDef USE_CPU16}
+function sc_StrToWord(const Str: String; out Value: Word): Boolean;
+function s_StrToWord(const Str: String): Word;
+{$EndIf}
+{$IfDef USE_CPU32}
+function sc_StrToLongWord(const Str: String; out Value: LongWord): Boolean;
+function s_StrToLongWord(const Str: String): LongWord;
+{$EndIf}
+{$IfDef USE_CPU64}
+function sc_StrToQWord(const Str: String; out Value: QWord): Boolean;
+function s_StrToQWord(const Str: String): QWord;
+{$EndIf}
+
+implementation
+
+var
+  allIntParametr, allUIntParametr: array[0..7] of geUseParametr;
+  allHOBParametr: array[0..3] of geHOBParameter;
+  IntMinus: Boolean;                         // указывает знак минуса
+
+// Rus: выставляем необходимые параметры. Иначе geStrToInt работать не будет.
+//      В блоке инициализации.
+// Eng: set the required parameters. Otherwise geStrToInt won't work.
+//      in the initialization block.
+procedure SetNumberParametr;
+begin
+  allUIntParametr[isByte].maxLen := 3;
+  allUIntParametr[isByte].maxNumeric := 255;
+  allUIntParametr[isByte].maxNumDiv10 := 25;
+  allIntParametr[isShortInt].maxLen := 4;
+  allIntParametr[isShortInt].maxNumeric := 127;
+  allIntParametr[isShortInt].maxNumDiv10 := 12;
+  {$IfDef USE_CPU16}
+  allUIntParametr[isWord].maxLen := 5;
+  allUIntParametr[isWord].maxNumeric := 65535;
+  allUIntParametr[isWord].maxNumDiv10 := 6553;
+  allIntParametr[isSmallInt].maxLen := 6;
+  allIntParametr[isSmallInt].maxNumeric := 32767;
+  allIntParametr[isSmallInt].maxNumDiv10 := 3276;
+  {$EndIf}
+  {$IfDef USE_CPU32}
+  allUIntParametr[isLongWord].maxLen := 10;
+  allUIntParametr[isLongWord].maxNumeric := 4294967295;
+  allUIntParametr[isLongWord].maxNumDiv10 := 429496729;
+  allIntParametr[isInteger].maxLen := 11;
+  allIntParametr[isInteger].maxNumeric := 2147483647;
+  allIntParametr[isInteger].maxNumDiv10 := 214748364;
+  {$EndIf}
+  {$IfDef USE_CPU64}
+  allUIntParametr[isQWord].maxLen := 20;
+  allUIntParametr[isQWord].maxNumeric := 18446744073709551615;
+  allUIntParametr[isQWord].maxNumDiv10 := 1844674407370955161;
+  allIntParametr[isInt64].maxLen := 20;
+  allIntParametr[isInt64].maxNumeric := 9223372036854775807;
+  allIntParametr[isInt64].maxNumDiv10 := 922337203685477580;
+  {$EndIf}
+
+  allHOBParametr[isByte].maxLen16 := 3;
+  allHOBParametr[isByte].maxLen8 := 4;
+  allHOBParametr[isByte].maxLen2 := 9;
+  {$IfDef USE_CPU16}
+  allHOBParametr[isWord].maxLen16 := 5;
+  allHOBParametr[isWord].maxLen8 := 7;
+  allHOBParametr[isWord].maxLen2 := 17;
+  {$EndIf}
+  {$IfDef USE_CPU32}
+  allHOBParametr[isLongWord].maxLen16 := 9;
+  allHOBParametr[isLongWord].maxLen8 := 12;
+  allHOBParametr[isLongWord].maxLen2 := 33;
+  {$EndIf}
+  {$IfDef USE_CPU64}
+  allHOBParametr[isQWord].maxLen16 := 17;
+  allHOBParametr[isQWord].maxLen8 := 23;
+  allHOBParametr[isQWord].maxLen2 := 65;
+  {$EndIf}
+end;
+
+function sc_StrToShortInt(const Str: String; out Value: ShortInt): Boolean;
+var
+  n: maxIntVal;
+begin
+  Result := geStrToInt(Str, n, isShortInt);
+  Value := n;
+end;
+
+function s_StrToShortInt(const Str: String): ShortInt;
+var
+  n: maxIntVal;
+begin
+  geStrToInt(Str, n, isShortInt);
+  Result := n;
+end;
+
+{$IfDef USE_CPU16}
+function sc_StrToSmallInt(const Str: String; out Value: SmallInt): Boolean;
+var
+  n: maxIntVal;
+begin
+  Result := geStrToInt(Str, n, isSmallInt);
+  Value := n;
+end;
+
+function s_StrToSmallInt(const Str: String): SmallInt;
+var
+  n: maxIntVal;
+begin
+  geStrToInt(Str, n, isSmallInt);
+  Result := n;
+end;
+{$EndIf}
+
+{$IfDef USE_CPU32}
+function sc_StrToInt(const Str: String; out Value: Integer): Boolean;
+var
+  n: maxIntVal;
+begin
+  Result := geStrToInt(Str, n, isInteger);
+  Value := n;
+end;
+
+function s_StrToInt(const Str: String): Integer;
+var
+  n: maxIntVal;
+begin
+  geStrToInt(Str, n, isInteger);
+  Result := n;
+end;
+{$EndIf}
+
+{$IfDef USE_CPU64}
+function sc_StrToInt64(const Str: String; out Value: Int64): Boolean;
+begin
+  Result := geStrToInt(Str, Value, isInt64);
+end;
+
+function s_StrToInt64(const Str: String): Int64;
+begin
+  geStrToInt(Str, Result, isInt64);
+end;
+
+{$EndIf}
+
+function sc_StrToByte(const Str: String; out Value: Byte): Boolean;
+var
+  n: maxUIntVal;
+begin
+  n := Byte(Str[1]);
+  if (n = 48) or ((n >= 36) and (n <= 38)) then
+    Result := geHOBStrToUInt(str, n, isByte)
+  else
+    Result := geStrToUInt(Str, n, isByte);
+  Value := n;
+end;
+
+function s_StrToByte(const Str: String): Byte;
+var
+  n: maxUIntVal;
+begin
+  n := Byte(Str[1]);
+  if (n = 48) or ((n >= 36) and (n <= 38)) then
+    geHOBStrToUInt(str, n, isByte)
+  else
+    geStrToUInt(Str, n, isByte);
+  Result := n;
+end;
+
+{$IfDef USE_CPU16}
+function sc_StrToWord(const Str: String; out Value: Word): Boolean;
+var
+  n: maxUIntVal;
+begin
+  n := Byte(Str[1]);
+  if (n = 48) or ((n >= 36) and (n <= 38)) then
+    Result := geHOBStrToUInt(str, n, isWord)
+  else
+    Result := geStrToUInt(Str, n, isWord);
+  Value := n;
+end;
+
+function s_StrToWord(const Str: String): Word;
+var
+  n: maxUIntVal;
+begin
+  n := Byte(Str[1]);
+  if (n = 48) or ((n >= 36) and (n <= 38)) then
+    geHOBStrToUInt(str, n, isWord)
+  else
+    geStrToUInt(Str, n, isWord);
+  Result := n;
+end;
+{$EndIf}
+
+{$IfDef USE_CPU32}
+function sc_StrToLongWord(const Str: String; out Value: LongWord): Boolean;
+var
+  n: maxUIntVal;
+begin
+  n := Byte(Str[1]);
+  if (n = 48) or ((n >= 36) and (n <= 38)) then
+    Result := geHOBStrToUInt(str, n, isLongWord)
+  else
+    Result := geStrToUInt(Str, n, isLongWord);
+  Value := n;
+end;
+
+function s_StrToLongWord(const Str: String): LongWord;
+var
+  n: maxUIntVal;
+begin
+  n := Byte(Str[1]);
+  if (n = 48) or ((n >= 36) and (n <= 38)) then
+    geHOBStrToUInt(str, n, isLongWord)
+  else
+    geStrToUInt(Str, n, isLongWord);
+  Result := n;
+end;
+{$EndIf}
+
+{$IfDef USE_CPU64}
+function sc_StrToQWord(const Str: String; out Value: QWord): Boolean;
+var
+  n: maxUIntVal;
+begin
+  n := Byte(Str[1]);
+  if (n = 48) or ((n >= 36) and (n <= 38)) then
+    Result := geHOBStrToUInt(str, Value, isQWord)
+  else
+    Result := geStrToUInt(Str, Value, isQWord);
+end;
+
+function s_StrToQWord(const Str: String): QWord;
+var
+  n: maxUIntVal;
+begin
+  n := Byte(Str[1]);
+  if (n = 48) or ((n >= 36) and (n <= 38)) then
+    geHOBStrToUInt(str, Result, isQWord)
+  else
+    geStrToUInt(Str, Result, isQWord);
+end;
+{$EndIf}
+
+function geStrToUInt(const Str: String; out Value: maxUIntVal; Size: LongWord = isLongWord): Boolean;
+var
+  lenStr, i: maxUIntVal;
+  m, n, z: maxUIntVal;
+begin
+  {$push}
+  {$Q-}{$R-}
+  // Rus: значение нуль, результат функции - False.
+  // Eng: the value is zero, the result of the function is False.
+  Result := False;
+  Value := 0;
+  if Size > maxSize then
+    Exit;
+  lenStr := Length(Str);
+  if lenStr = 0 then
+    exit;
+  m := Byte(Str[1]) - 48;
+  // Rus: если значение не входит в пределы 0..9 - выходим
+  // Eng: if the value is outside the range 0..9 - exit
+  if m > 9 then
+    exit;
+  if (lenStr = 1) and (m = 0) then
+  begin
+    Result := True;
+    exit;
+  end;
+
+  if lenStr > allUIntParametr[Size].maxLen then
+    Exit;
+  i := 2;
+  while i < lenStr do
+  begin
+    n := (Byte(Str[i]) - 48);
+    if n > 9 then
+      Exit;
+    m := m * 10 + n;
+    inc(i);
+  end;
+  // Rus: Если уже превысили значение, то выходим
+  // Eng: If you have already exceeded the value, then exit
+  if m > allUIntParametr[Size].maxNumDiv10 then
+    exit;
+  m := m * 10;
+  z := Byte(Str[i]) - 48;
+  if z > 9 then
+    exit;
+  n := allUIntParametr[Size].maxNumeric - m;
+  if z > n then
+    exit;
+  Value := m + z;
+  Result := true;
+  {$pop}
+end;
+
+function geHOBStrToUInt(const Str: String; out Value: maxUIntVal; Size: LongWord = isQWord): Boolean;
+var
+  lenStr, i: maxUIntVal;
+  m, n, nshl, z: maxUIntVal;
+  correct: maxUIntVal = 0;
+label
+  jmpExit;
+begin
+  {$push}
+  {$Q-}{$R-}
+  Result := False;
+  Value := 0;
+  if Size > maxSize then
+    exit;
+  lenStr := Length(Str);
+  if lenStr = 0 then
+    exit;
+
+  // первые значения влияют на рабочий код. Потому что здесь будут использоваться
+  // и шестнадцатеричные и восьмеричные и двоичные значения.
+  // Числа надо обрабатывать соответственно $(0x) - шестнадцатеричный,
+  // %(0b) - двоичный, &(0) - восьмеричный
+  // Так же учитываем при работе с не десятичными числами, что перед ними идут
+  // дополнительные значения, а значит длина их должна быть больше на 1 или 2.
+  // Выставляю на 1 больше, что означает, что при первом нуле, вероятно надо
+  // будет указать ещё дополнительную длину.
+
+  // Rus: здесь обработка первого символа, который указывает на систему счисления.
+  // Eng: here processing the first character, which indicates the number system.
+  m := Byte(Str[1]);
+  n := Byte(Str[2]);
+  i := 2;
+  if m = 48 then
+  begin
+    if n > 97 then
+      n := n - 32;
+    if (n = 66) or (n = 88) then
+    begin
+      correct := 1;
+      i := 3;
+      if n = 66 then
+      begin
+        m := 37;
+        z := 1;
+      end
+      else
+        m := 36;
+    end
+    else begin
+      m := 38;
+      z := 7;
+    end;
+  end;
+
+  // Rus: проверим предел для восьмеричной системы.
+  // Eng: check the limit for the octal system.
+  if m = 38 then
+  begin
+    n := n - 48;
+    if lenStr > allHOBParametr[Size].maxLen8 then
+      exit;
+    if allHOBParametr[Size].maxLen8 = lenStr then       // предельная длина
+      if (Size = isByte) or (Size = isLongWord) then
+      begin
+        if n > 3 then                                   // max 3
+          exit;
+      end
+      else
+        if n > 1 then                                   // max 1
+          exit;
+    nshl := 3;
+  end
+  else
+    if m = 37 then
+    begin
+      // Rus: для двоичной системы.
+      // Eng: for a binary system.
+      if (lenStr - correct) > allHOBParametr[Size].maxLen2 then
+        exit;
+      nshl := 1;
+    end;
+  // Rus: вычисляем шестнадцатеричное число.
+  // Eng: calculate a hexadecimal number.
+  if m = 36 then
+  begin
+    if (lenStr - correct) > allHOBParametr[Size].maxLen16 then
+      exit;
+    m := 0;
+    while i <= lenStr do
+    begin
+      m := m shl 4;       // возможно ли что это место выдаст когда-нибудь ошибку?
+      n := Byte(Str[i]);
+      case n of
+          48..57: m := m + n - 48;
+          65..70: m := m + n - 55;
+          97..102: m := m + n - 87;
+        else
+          exit;
+      end;
+      inc(i);
+    end;
+  end
+  // Rus: вычисляем двоичное или восьмеричное число.
+  // Eng: calculate a binary or octal number.
+  else begin
+    m := Byte(Str[i]) - 48;
+    if m > z then
+      exit;
+    inc(i);
+    while i <= lenStr do
+    begin
+      m := m shl nshl;
+      n := Byte(Str[i]) - 48;
+      if n > z then
+        exit;
+      m := m + n;
+      inc(i);
+    end;
+  end;
+  Value := m;
+  Result := true;
+  {$pop}
+end;
+
+function geStrToInt(const Str: String; out Value: maxIntVal; Size: LongWord = isInteger): Boolean;
+var
+  lenStr, i: maxUIntVal;
+  m, n, z: maxUIntVal;
+label
+  jmpEndStr;
+begin
+  {$push}
+  {$Q-}{$R-}
+  // Rus: значение нуль, результат функции - False.
+  // Eng: the value is zero, the result of the function is False.
+  Result := False;
+  Value := 0;
+  if Size > maxSize then
+    Exit;
+  // Rus: флаг изначально указан, что не действителен.
+  // Eng: the flag is initially specified, which is not valid.
+  IntMinus := False;
+  lenStr := Length(Str);
+  if lenStr = 0 then
+    exit;
+  i := 1;
+  m := Byte(Str[i]);
+
+  if (lenStr = 1) and (m = 48) then
+  begin
+    Result := True;
+    exit;
+  end;
+  // Rus: если рассматриваем отридцательные тоже, то проверяем знак
+  // Eng: if we consider negative ones too, then we check the sign
+  if m = 45 then
+  begin
+    if lenStr = 1 then
+      exit;
+    IntMinus := True;
+    inc(i);
+    m := Byte(Str[2]);
+  end;
+
+  inc(i);
+  m := m - 48;
+  // Rus: если значение не входит в пределы 0..9 - выходим
+  // Eng: if the value is outside the range 0..9 - exit
+  if m > 9 then
+    exit;
+
+  // Rus: момент, когда длина на один символ (символ + знак)
+  // Eng: moment when the length is one character (symbol + sign)
+  if i > lenStr then
+  begin
+    z := 0;
+    goto jmpEndStr;
+  end;
+  // Rus: проверяем длину для данной размерности.
+  // Eng: check the length for a given dimension.
+  if lenStr > allIntParametr[Size].maxLen then
+    Exit;
+  while i < lenStr do
+  begin
+    n := (Byte(Str[i]) - 48);
+    if n > 9 then
+      Exit;
+    m := m * 10 + n;
+    inc(i);
+  end;
+
+  // Rus: Если уже превысили значение, то выходим
+  // Eng: If you have already exceeded the value, then exit
+  if m > allIntParametr[Size].maxNumDiv10 then
+    exit;
+  m := m * 10;
+  z := Byte(Str[i]) - 48;
+  if z > 9 then
+    exit;
+
+jmpEndStr:
+  if IntMinus then
+    n := allIntParametr[Size].maxNumeric + 1 - m
+  else
+    n := allIntParametr[Size].maxNumeric - m;
+  if z > n then
+    exit;
+
+  if IntMinus then
+    Value := - m - z
+  else
+    Value := m + z;
+  Result := true;
+  {$pop}
+end;
+
+initialization
+
+  SetNumberParametr;
+
+end.
+
---
+++ geStrToInt/project1.lpr
@@ -0,0 +1,279 @@
+program project1;
+
+{$APPTYPE CONSOLE}
+uses
+  {SysUtils,}{$IfDef UNIX}UnixType,{$EndIf} {$IfDef Windows} Windows,{$EndIf}ge_external_Utils;
+
+const
+  MAX_TEST = 100000;
+  Max_T_DIV10 = 10000;
+
+var
+//  myS: string[20] = '-30493104565373';
+  Count, Error: Integer;
+  timeStart, timeEnd, timerStart: Double;
+  {$IfDef UNIX}{$IfNDef MAC_COCOA}
+  timerTimeVal: TimeVal;
+  {$Else}
+  timerTimebaseInfo: mach_timebase_info_t;
+  {$ENDIF}
+  {$ENDIF}
+  {$IFDEF WINDOWS}
+  timerFrequency: Int64;
+  {$ENDIF}
+  timeOld, timeOld2, timeNew, timeNew2: Double;
+  myS: array[0..MAX_TEST] of String[20];
+  myNumb: array[0..MAX_TEST] of Integer;
+  nnnn: Int64;
+  mmmm: QWord;
+  mn: Byte;
+
+  {$IfDef UNIX}{$IfNDef MAC_COCOA}
+  function fpGetTimeOfDay(val: PTimeVal; tzp: Pointer): Integer; cdecl; external 'libc' name 'gettimeofday';
+  {$Else}
+  type
+    mach_timebase_info_t = record
+      numer: LongWord;
+      denom: LongWord;
+    end;
+
+    function mach_timebase_info(var info: mach_timebase_info_t): Integer; cdecl; external 'libc';
+    function mach_absolute_time: QWORD; cdecl; external 'libc';
+  {$ENDIF}{$EndIf}
+
+// запрос таймера, оставляю для всех систем
+function timer_GetTicks: Double;
+{$IFDEF WINDOWS}
+var
+  t: int64;
+  m: LongWord;
+{$EndIf}
+begin
+  {$IfDef UNIX}{$IfNDef MAC_COCOA}
+  fpGetTimeOfDay(@timerTimeVal, nil);
+  {$Q-}
+  // FIXME: почему-то overflow вылетает с флагом -Co
+  Result := timerTimeVal.tv_sec * 1000 + timerTimeVal.tv_usec / 1000 - timerStart;
+  {$Q+}
+{$Else}
+  Result := mach_absolute_time() * timerTimebaseInfo.numer / timerTimebaseInfo.denom / 1000000 - timerStart;
+{$ENDIF}{$EndIf}
+{$IFDEF WINDOWS}
+  m := SetThreadAffinityMask(GetCurrentThread(), 1);
+  QueryPerformanceCounter(t);
+  Result := 1000 * t / timerFrequency - timerStart;
+  SetThreadAffinityMask(GetCurrentThread(), m);
+{$ENDIF}
+end;
+
+function u_StrToInt(const Value: UTF8String): Integer;
+  var
+    e: Integer;
+begin
+  Val(Value, Result, e);
+  if e <> 0 Then
+    Result := 0;
+end;
+
+function u_IntToStr(Value: Integer): String;
+begin
+  Str(Value, Result);
+end;
+
+// создаём массив чисел
+procedure CreateArrayNumb;
+var
+  i: Integer;
+begin
+  Randomize;
+  for i := 0 to Max_T_DIV10 - 1 do
+  begin
+    myNumb[i * 10] := random(High(Integer));
+    myNumb[i * 10 + 1] := - myNumb[i * 10];
+    myNumb[i * 10 + 2] := myNumb[i * 10] mod 10;
+    myNumb[i * 10 + 3] := myNumb[i * 10] div 10;
+    myNumb[i * 10 + 4] := myNumb[i * 10] mod 100;
+    myNumb[i * 10 + 5] := myNumb[i * 10] div 100;
+    myNumb[i * 10 + 6] := - myNumb[i * 10 + 2];
+    myNumb[i * 10 + 7] := - myNumb[i * 10 + 3];
+    myNumb[i * 10 + 8] := - myNumb[i * 10 + 4];
+    myNumb[i * 10 + 9] := - myNumb[i * 10 + 5];
+    myS[i * 10] := u_IntToStr(myNumb[i * 10]);
+    myS[i * 10 + 1] := u_IntToStr(myNumb[i * 10 + 1]);
+    myS[i * 10 + 2] := u_IntToStr(myNumb[i * 10 + 2]);
+    myS[i * 10 + 3] := u_IntToStr(myNumb[i * 10 + 3]);
+    myS[i * 10 + 4] := u_IntToStr(myNumb[i * 10 + 4]);
+    myS[i * 10 + 5] := u_IntToStr(myNumb[i * 10 + 5]);
+    myS[i * 10 + 6] := u_IntToStr(myNumb[i * 10 + 6]);
+    myS[i * 10 + 7] := u_IntToStr(myNumb[i * 10 + 7]);
+    myS[i * 10 + 8] := u_IntToStr(myNumb[i * 10 + 8]);
+    myS[i * 10 + 9] := u_IntToStr(myNumb[i * 10 + 9]);
+  end;
+end;
+
+begin
+{$IFDEF WINDOWS}
+  SetThreadAffinityMask(GetCurrentThread(), 1);
+  QueryPerformanceFrequency(timerFrequency);
+{$ENDIF}
+{$IFDEF MAC_COCOA}
+  mach_timebase_info(timerTimebaseInfo);
+{$ENDIF}
+  timerStart := timer_GetTicks();
+  timeOld := 0;
+  timeNew := 0;
+  timeOld2 := 0;
+  timeNew2 := 0;
+  CreateArrayNumb;
+
+  // В первые секунды и при первом запуске, выводится неправильное значение. С чем это связано?
+  // In the first seconds and at the first start, an incorrect value is displayed. What is the reason for this?
+   // ----------------------------------------------
+  writeln('Wait...');
+  timeStart := timer_GetTicks;
+  for Count := 0 to MAX_TEST * 100 do
+  begin
+    geStrToInt(myS[1], nnnn);
+  end;
+  timeEnd := timer_GetTicks - timeStart;
+
+  timeStart := timer_GetTicks;
+  for Count := 0 to MAX_TEST * 10 do
+  begin
+    u_StrToInt(myS[1]);
+  end;
+  timeEnd := timer_GetTicks - timeStart;
+  Writeln;
+
+  Writeln('Test begin.');
+  // Сделаем цикл, для проверки
+  for Error := 0 to 100 do
+  begin
+    // ----------------------------------------------
+    timeStart := timer_GetTicks;
+    for Count := 0 to MAX_TEST do
+    begin
+      nnnn := u_StrToInt(myS[Count]);
+//      mmmm := u_StrToInt('$FFFFffff');
+    end;
+    timeEnd := timer_GetTicks - timeStart;
+    timeOld := timeOld + timeEnd;
+
+    // ----------------------------------------------
+    timeStart := timer_GetTicks;
+    for Count := 0 to MAX_TEST do
+    begin
+      geStrToInt(myS[Count], nnnn);
+//      geHOBStrToUInt('$FFFFffff', mmmm);
+    end;
+    timeEnd := timer_GetTicks - timeStart;
+    timeNew := timeNew + timeEnd;
+
+    // ----------------------------------------------
+
+{    timeStart := timer_GetTicks;
+    for Count := 0 to MAX_TEST do
+    begin
+      u_IntToStr(myNumb[Count]);
+    end;
+    timeEnd := timer_GetTicks - timeStart;
+    timeNew2 := timeNew2 + timeEnd;
+
+    timeStart := timer_GetTicks;
+    for Count := 0 to MAX_TEST do
+    begin
+      u_IntToStr(myNumb[Count]);
+    end;
+    timeEnd := timer_GetTicks - timeStart;
+    timeOld2 := timeOld2 + timeEnd;       }
+    // ----------------------------------------------
+  end;
+  Write('StrToInt standard    ');
+  Writeln(timeOld / 101);
+
+  Write('StrToInt made by me ');
+  Writeln(timeNew / 101);
+
+{  Write('Test Speed Single and Integer ');
+  Writeln(timeOld2 / 101);
+
+  Write('Test Speed Single all         ');
+  Writeln(timeNew2 / 101);}
+  Writeln('Test end.');
+
+  WriteLn;
+  WriteLn(nnnn);
+
+  WriteLn;
+  WriteLn('Numbers:');
+  for Count := 0 to 17 do
+  begin
+    geStrToInt(myS[Count], nnnn);
+    WriteLn(nnnn);
+  end;
+
+  // Rus: здесь примеры использования с разными флагами.
+  // Eng: here are examples of use with different flags.
+  // !!! geStrToUInt <> geStrToInt !!!
+  WriteLn;
+  Writeln('Examples');
+  WriteLn('QWord');
+  geStrToUInt('18446744073709551615', mmmm, isQWord);
+  WriteLn(mmmm);
+
+  Writeln;
+  WriteLn('Int64');
+  geStrToInt('-9223372036854775808', nnnn, isInt64);
+  WriteLn(nnnn);
+
+  Writeln;
+  WriteLn('Byte');
+  // Rus: пример использования байта. Если вы задаёте флаг байта, то из resQWord вы должны прочитать байт.
+  //      Это так же работает и для Word и для LongWord.
+  //      Если ваши данные это ShortInt, SmallInt или Integer вы должны будете читать данные из resInt64 в соответствующем размере.
+  //      данные не будут превышены, максимально заданного значения. Смотрите флаги в ge_external_Utils.
+  // Eng: an example of using a byte. If you set the byte flag, then you must read the byte from resQWord.
+  //      This works the same for Word and LongWord.
+  //      If your data is ShortInt, SmallInt or Integer you will need to read the data from resInt64 at the appropriate size.
+  //      data will not exceed the maximum specified value. See flags at ge_external_Utils.
+  if geStrToInt('$250', nnnn, isShortInt) then
+    WriteLn(nnnn)
+  // will throw an error if the value is greater than the byte value
+  else
+    WriteLn('Error');
+  if geStrToUInt('250', mmmm, isByte) then
+    WriteLn(mmmm)
+  // will throw an error if the value is greater than the byte value
+  else
+    WriteLn('Error');
+
+  WriteLn();
+  // Rus: здесь работаем с шестнадцатеричными, двоичными и восьмеричными значениями.
+  //      для шестнадцатеричных значений могут использоваться как '$' так и '0X'
+  //      или '0x'; для восьмеричных значений могут использоваться как '&' так и '0'
+  //      (одиночный нуль); для двоичных значений могут использоваться как '%' так
+  //      и '0B' или '0b'.
+  // Eng: here we work with hexadecimal, binary and octal values. for hexadecimal
+  //      values either '$' or '0X' or '0x' can be used; for octal values both '&'
+  //      and '0' (single zero) can be used; for binary values either '%' or '0B'
+  //      or '0b' can be used.
+  if geHOBStrToUInt('$10', mmmm, isByte) then
+    WriteLn(mmmm)
+  else
+    WriteLn('Error');
+
+  mn := s_StrToByte('0xff');
+  WriteLn(mn);
+
+  if sc_StrToQWord('$1ffffffffffffffff', mmmm) then       // remove 1 at the beginning
+    WriteLn(mmmm)
+  // will throw an error if the value is greater than the byte value
+  else
+    WriteLn('Error');
+(*  mmmm := 18446744073709551615;   // этот код был для проверки
+
+  mmmm := mmmm + 5;
+  WriteLn(mmmm); *)
+
+  Readln;
+end.
